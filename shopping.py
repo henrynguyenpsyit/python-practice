19838e34015d69275e7e40070fddce1ff240d1b9
Cà <henrynguyen0698@gmail.com>
2026-05-20 18:07:05 +0700

    Add edit price, custom style, duplicate check

--- shopping.py
+++ shopping.py
@@ -1,9 +1,17 @@
 import questionary
 import os
 import json
+from questionary import Style
+
+custom_style = Style([
+    ("pointer", "fg:#00ff00 bold"),
+    ("highlighted", "fg:#00ff00 bold"),
+    ("selected", "fg:#cc5454"),
+])
 
 FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH = os.path.join(FOLDER,"shopping_cart.json")
+
 
 DEFAULTS = {
     "item": "",
@@ -35,25 +43,40 @@ def check_cart(cart):
     
     bought_items = [c for c in cart if c["bought"]]
     unbought_items = [c for c in cart if not c["bought"]]
-
-    print(f"\n🛒 ĐÃ MUA {len(bought_items)}")
-    for c in bought_items:
-        print(f" ✓  {c['item']} - {c['price']} VND")
-    
     total_bought = sum(c["price"] for c in cart if c["bought"])
-    print(f"\n ĐÃ MUA: {total_bought} VND")
+    total_unbought = sum(c["price"] for c in cart if not c["bought"])
 
-    print(f"\n📋 CHƯA MUA {len(unbought_items)}")
+    print(f"\n🛒 ĐÃ CÓ TRONG GIỎ ({len(bought_items)}) - {total_bought:,} VND")
+    for c in bought_items:
+        print(f" ✓  {c['item']} - {c['price']:,} VND")
+
+
+    print(f"\n📋 CHƯA CÓ TRONG GIỎ ({len(unbought_items)}) - {total_unbought:,} VND")
     for i, c in enumerate(cart, 1):
         if not c["bought"]:
-            print(f" {i}. {c['item']} - {c['price']} VND")
+            print(f" {i}. {c['item']} - {c['price']:,} VND")
 
-    total_unbought = sum(c["price"] for c in cart if not c["bought"])
-    print(f"\n CHƯA MUA: {total_unbought} VND")
      
     total = sum(c["price"] for c in cart)
-    print(f"\n💰 TỔNG SỐ TIỀN CẢ GIỎ HÀNG: {total} VND")
+    print(f"\n💰 TỔNG SỐ TIỀN CẢ GIỎ HÀNG: {total:,} VND")
 
+
+def check_out(cart):
+    total_bought = sum(c["price"] for c in cart if c["bought"])
+    if total_bought == 0:
+        print("Chưa có món nào trong giỏ hàng")
+        return
+    pocket = ask_int("Số tiền mang theo: ", min_value=0)
+    print(f"> Bạn mang theo: {pocket:,} VND")
+    print(f">>Tổng đã mua: {total_bought:,} VND")
+    change = pocket - total_bought
+    if change > 0:
+        print(f"→ Tiền thừa: {change:,} VND")
+    elif change < 0:
+        print(f"→ Còn thiếu: {abs(change):,} VND")
+    elif change == 0:
+        print("→ Vừa đủ tiền!")
+    
 
 def mark_bought(cart, index):
     if not (0 < index <= len(cart)):
@@ -101,28 +124,33 @@ def load_cart():
 def main():
     cart = load_cart()
     ADD = "Thêm vào giỏ hàng"
-    CHECK = "Kiểm tra giỏ hàng"
+    TOTAL = "Kiểm tra giỏ hàng"
+    EDIT = "Chỉnh sửa giá"
     MARK = "Đánh dấu đã mua"
+    CHECK = "Tính tiền"
     DEL = "Xoá món hàng"
     CLEAR = "Xoá toàn bộ"
     EXIT = "Thoát"
     while True:
         choice = questionary.select(
             "Chọn mục:",
-            choices = [ADD, CHECK, MARK, DEL, CLEAR, EXIT]
+            choices = [ADD, TOTAL, EDIT, MARK, DEL, CHECK, CLEAR, EXIT],
+            style=custom_style
         ). ask()
-        print(choice)
         
         if choice == ADD:
             name = input("Thêm món hàng cần mua: ").strip()
             if not name:
                 print("Tên không được rỗng!")
                 continue
+            if any (c['item'].lower() == name.lower() for c in cart):
+                print(f"{name} đã có trong giỏ. Hãy chỉnh sửa giá!")
+                continue
             price = ask_int("Giá: ", min_value=0)
             add_item(cart, name, price)
             save_cart(cart)
 
-        elif choice == CHECK:
+        elif choice == TOTAL:
             check_cart(cart)
         
         elif choice == MARK:
@@ -141,7 +169,7 @@ def main():
             check_cart(cart)
             dlt = ask_int("Nhập thứ tự món muốn xoá: ")
             if not (0 < dlt <= len(cart)):
-                print("Lỗi. Vui lòng nhập lại")
+                print("Lỗi. Vui lòng nhập lại!")
                 continue
             confirm = questionary.confirm(f"Bạn chắc chắn muốn xoá {cart[dlt-1]['item']}?").ask()
             if confirm:
@@ -151,6 +179,9 @@ def main():
             else:
                 print("Huỷ xoá")
 
+        elif choice == CHECK:
+            check_out(cart)
+
         elif choice == CLEAR:
             cnfrm = questionary.confirm(f"Bạn chắc chắn muốn xoá toàn bộ giỏ hàng?").ask()
             if cnfrm:
@@ -159,7 +190,23 @@ def main():
                 print("Đã xoá toàn bộ giỏ hàng!")
             else:
                 print("Huỷ bỏ")
-
+        
+        elif choice == EDIT:
+            check_cart(cart)
+            index = ask_int("Sửa giá mặt hàng: ")
+            if not (0 < index <= len(cart)):
+                print(f"Không hợp lệ. Chỉ có {len(cart)} món!")
+                continue
+            item=cart[index-1]
+            confirm_index = questionary.confirm(f"Sửa giá {item['item']} (hiện đang {item['price']:,})?").ask()
+            if not confirm_index:
+                print("Huỷ bỏ")
+                continue
+            new_price = ask_int("Giá mới: ", min_value=0)
+            item["price"] = new_price
+            save_cart(cart)
+            print(f"Đã sửa: {item['item']} = {new_price:,} VND")
+            
         elif choice == EXIT:
             print("Chương trình kết thúc. Tạm biệt!")
             break
